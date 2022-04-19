53a09a1071a393c32d2a52c2a8536d7cfdfa1e31
geetaofficial <93854018+geetaofficial@users.noreply.github.com>
2022-04-19 11:57:37 +0530

    db changes

--- zapbuild/settings.py
+++ zapbuild/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'zapbuild.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'zapbuild_db',
-#         'USER': 'root',
-#         'PASSWORD': 'root12345',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zapbuild_db',
+        'USER': 'root',
+        'PASSWORD': 'root12345',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
